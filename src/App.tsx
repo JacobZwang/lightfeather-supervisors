84912f2fe16f4606f84dc55b445a7e2eb036e932
JacobZwang <zwang.jacob@gmail.com>
2022-05-27 12:04:13 -0700

    your info markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import './App.css';
 
 function App() {
-	return <p>hello react</p>;
+	return (
+		<form>
+			<div>
+				<h2>Your Information</h2>
+				<label>
+					First Name
+					<input required type="text" name="First Name" placeholder="Jacob" />
+				</label>
+
+				<label>
+					Last Name
+					<input required type="text" name="Last Name" placeholder="Zwang" />
+				</label>
+
+				<h2>Notifications</h2>
+
+				<label>
+					Email
+					<input type="text" name="Last Name" placeholder="youremail@lightfeather.io" />
+				</label>
+
+				<label>
+					Phone Number
+					<input type="text" name="Last Name" placeholder="123 456 7890" />
+				</label>
+
+				<br />
+
+				<div
+					style={{
+						textAlign: 'center'
+					}}
+				>
+					<label className="checkbox">
+						Save Options
+						<input type="checkbox" checked></input>
+					</label>
+				</div>
+			</div>
+		</form>
+	);
 }
 
 export default App;
