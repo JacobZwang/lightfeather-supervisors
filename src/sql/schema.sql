879ac4d84aaa331044f05a2731fc5a4557755b19
JacobZwang <zwang.jacob@gmail.com>
2022-05-28 10:04:26 -0700

    set up postgres and schema

---
+++ src/sql/schema.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS supervisor_email_notifications (
+    email TEXT NOT NULL,
+    name_first TEXT NOT NULL,
+    name_last TEXT NOT NULL,
+    supervisor TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS supervisor_phone_notifications (
+    email TEXT NOT NULL,
+    name_first TEXT NOT NULL,
+    name_last TEXT NOT NULL,
+    supervisor TEXT NOT NULL
+);
+
+COMMIT;
